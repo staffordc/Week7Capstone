ed62d2e96583519261d0ff60b0d98c4ac0e732d3
staffordc <stafford.carh@gmail.com>
2018-08-31 16:34:43 -0400

    Added the VM not a way to connect to it

---
+++ Week7Capstone/ViewModels/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Week7Capstone.DAL;
+
+namespace Week7Capstone.ViewModels
+{
+    public class ProductVM
+    {
+        public Product productName { get; set; }
+        public Product productId { get; set; }
+    }
+}
